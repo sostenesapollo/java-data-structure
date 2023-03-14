d403a25037ce7126d1f31d35b32d9573d5d65fe0
Sóstenes Apollo <ctr.Sostenes.DaSilva@prth.com>
2023-03-14 09:17:32 -0300

    chore: moved to BigO class

---
+++ src/BigO.java
@@ -0,0 +1,21 @@
+public class BigO {
+    public  static void main() {
+        printItems(14);
+    }
+    public static void printItems(int n) {
+        // O(nˆ2 + n) => O(nˆ2) => Drop dominant
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
+        for (int k = 0; k < n; k++) {
+            System.out.println(k);
+        }
+    }
+
+    public static int addItems(int n) {
+        // O(1) => Constant time (as n grows operations keep the same)
+        return n+n+n;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,6 @@
-//package misc.big_o;/**/
-
 public class Main {
     public static void main(String[] args) {
-        printItems(14);
-    }
-
-    public static int addItems(int n) {
-        // O(1) => Constant time (as n grows operations keep the same)
-        return n+n;
-    }
-
-    public static void printItems(int n) {
-        // O(nˆ2 + n) => O(nˆ2) => Drop dominant
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.println(i + " " + j);
-            }
-        }
-        for (int k = 0; k < n; k++) {
-            System.out.println(k);
-        }
+        BigO.printItems(12);
+        BigO.addItems(1);
     }
 }
