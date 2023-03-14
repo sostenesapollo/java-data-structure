ea304dade97cec2310962af4fea2011e6f462977
Sóstenes Apollo <ctr.Sostenes.DaSilva@prth.com>
2023-03-14 09:49:57 -0300

    chore: added printItemsWithTwoArgsMultiply

--- src/BigO.java
+++ src/BigO.java
@@ -16,4 +16,27 @@ public class BigO {
         // O(1) => Constant time (as n grows operations keep the same)
         return n+n+n;
     }
+
+    public static void printItemsWithTwoArgs(int a, int b) {
+        // O(a) + O(b) = O ( a + b ) <= As far as we can simplify this
+
+        // Because for i.e : O(1000000 + 1) it would consume a lot more of time
+
+        for (int i = 0; i < a; i++) {
+            System.out.println(i);
+        }
+
+        for (int j = 0; j < b; j++) {
+            System.out.println(j);
+        }
+    }
+
+    public static void printItemsWithTwoArgsMultiply(int a, int b) {
+        // O(a*b)
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                System.out.println(i+" "+j);
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        BigO.printItems(12);
-        BigO.addItems(1);
+//        BigO.printItems(12);
+//        BigO.addItems(1);
+
+        BigO.printItemsWithTwoArgs(4,10);
+        BigO.printItemsWithTwoArgsMultiply(1000, 3)
     }
 }
